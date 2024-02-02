beb9e3ae22b1670942e836d071adfc3c70c2642c
chanki <chanki.kim89@gmail.com>
2024-02-02 20:32:17 +0900

    오류수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,6 @@ import "../styles/global.css";
 import SWRConfigContext from "../context/SWRConfigContext";
 import { AuthContextProvider } from "@/context/AuthContext";
 import { Metadata } from "next";
-import AlertByQuery from "@/components/AlertByQuery";
 
 const roboto = Roboto({ weight: ["400", "500", "700"], subsets: ["latin"] });
 
@@ -17,7 +16,6 @@ export default function layout({ children }: { children: React.ReactNode }) {
     <html lang="ko">
       <body className={`app-layout ${roboto.className}`}>
         <div className="app-inner">
-          <AlertByQuery />
           <AuthContextProvider>
             <SWRConfigContext>{children}</SWRConfigContext>
           </AuthContextProvider>
