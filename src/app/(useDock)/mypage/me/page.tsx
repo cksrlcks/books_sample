e60b3e390f4ed8d4b37bddb0facf6512723fcef8
chanki <chanki.kim89@gmail.com>
2024-02-03 11:13:34 +0900

    마이페이지 프로필구역 변경

--- src/app/(useDock)/mypage/me/page.tsx
+++ src/app/(useDock)/mypage/me/page.tsx
@@ -2,5 +2,10 @@ import Inner from "@/components/Inner";
 import React from "react";
 
 export default function MyPage() {
-  return <Inner>마이페이지</Inner>;
+  return (
+    <Inner>
+      마이페이지 <br />
+      아바타, 이메일, 이름, 가입일, 회원탈퇴 구현
+    </Inner>
+  );
 }
