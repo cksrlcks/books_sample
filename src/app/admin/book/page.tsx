49a61af3ad54ab0d59fb9b401c8afacbf3854e4b
chanki <chanki.kim89@gmail.com>
2024-02-18 15:00:47 +0900

    관리자 책추가

--- src/app/admin/book/page.tsx
+++ src/app/admin/book/page.tsx
@@ -1,3 +1,11 @@
+import Link from "next/link";
+
 export default function Page() {
-  return <div>책관리페이지</div>;
+  return (
+    <div>
+      <Link href="/admin/book/add">책추가하기</Link>
+      <br />
+      책리스트
+    </div>
+  );
 }
